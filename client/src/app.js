de276c2733ca0e3e74a3f40507c1757dbb5e937c
Zack Zhu <zeyuzhu1@gmail.com>
2019-05-17 13:44:10 -0400

    Upgrade Babel to v7 and fix tests

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo-hooks';
+
+const GET_TOUR_DETAILS = gql`
+	{
+		getTour(id: 4) {
+			name
+			duration
+			description
+		}
+	}
+`;
 
 const App = () => {
-  return <div>Hello World!</div>;
+	const { data, error, loading } = useQuery(GET_TOUR_DETAILS);
+	if (loading) {
+		return <div>loading</div>;
+	}
+	if (error) {
+		return <div>Got an error: {error.message}</div>;
+	}
+
+	return (
+		<div>
+			<h1>Name: {data.getTour.name}</h1>
+			<h2>Duration: {data.getTour.duration}</h2>
+			<h2>Description: {data.getTour.description}</h2>
+		</div>
+	);
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import { ApolloProvider } from 'react-apollo-hooks';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+	uri: 'http://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('app')
+);
