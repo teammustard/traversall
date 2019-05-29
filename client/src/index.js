aa764495b72a2622d8968a0d2a5c17f8af3a67e3
Zack Zhu <zeyuzhu1@gmail.com>
2019-05-29 19:36:31 -0400

    Update graphQL uri

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:3000/graphql'
+	uri: 'http://localhost:3003/graphql'
 });
 
 // Hello
