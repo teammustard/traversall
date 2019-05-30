9c34c1fe4ecb82e01042e4764ab8acfa087de4a5
Zack Zhu <zeyuzhu1@gmail.com>
2019-05-30 15:02:47 -0400

    Fix search bar message

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 					<div className="c-search__input-wrap">
 						<form action="" id="search-form">
 							<span className="c-search__btn" />
-							<input type="text" className="c-search__input" placeholder="go away" />
+							<input type="text" className="c-search__input" placeholder="Where would you like to go?" />
 						</form>
 					</div>
 				</div>
