82632471d80491baadc9d6a4f8ac59919de4745e
Zack Zhu <zeyuzhu1@gmail.com>
2019-05-31 15:11:01 -0400

    Fix server

--- server/schema.js
+++ server/schema.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server-express');
 const fs = require('fs');
 const path = require('path');
 
-const schema = fs.readFileSync(path.join(__dirname, 'server/schema.graphql'), 'utf8').toString();
+const schema = fs.readFileSync(path.join(__dirname, '/schema.graphql'), 'utf8').toString();
 const typeDefs = gql`${schema}`;
 
 module.exports = {
